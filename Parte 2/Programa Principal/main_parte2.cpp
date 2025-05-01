e6b34caf64e3cc3e7a0d5dc70265138c2e8c7b33
Jimmy <jaimeperezzp@gmail.com>
2025-05-02 01:01:59 +0200

    Create main_parte2.cpp

---
+++ Parte 2/Programa Principal/main_parte2.cpp
@@ -0,0 +1,42 @@
+//main de la parte 2;
+
+#include "encabezado_parte2.h"
+#include "encabezado_general.h"
+
+//INTERRUPCIONES
+
+ISR(INT0_vect){			//PCINT para el SW1
+	
+	sw1_bajada();
+}
+
+ISR(INT1_vect){			//PCINT para el SW5
+	
+	sw5_bajada();
+}
+
+ISR(PCINT16){
+	
+	sw6_flanco();
+}
+
+ISR(TIMER0_COMPA_vect){
+	//esta interrupción salta cada 1 mS
+}
+
+//BUCLE 
+void loop(){
+	
+}
+
+//Programa Principal
+int main(void){
+		
+	setup_parte2();
+	
+	while(1){
+		
+		loop();
+	}
+
+}
