0e2e34ddbe4d1cd6d3b5cd3a4b8774ed5ed8493e
Jimmy <jaimeperezzp@gmail.com>
2025-05-02 00:09:38 +0200

    Update setup_timers.h
    He añadido el setup del timer 1 (De este, sale el PWM para los otros dos motores)

--- Setup Timers (PWMs)/setup_timers.h
+++ Setup Timers (PWMs)/setup_timers.h
@@ -1,7 +1,7 @@
 
-#define d_PWM5 250
-//Configuración timer 0. Sirve para contar hasta 1 mS
+#define d_PWM 250
 
+//Configuración timer 0. Sirve para contar hasta 1 mS
 void setup_timer0(){
 	
 	//Timer 0 para contar 1 mS 
@@ -18,8 +18,26 @@ void setup_timer0(){
 	
 }
 
-//Configuración del timer 5 (lo uso para el PWM de 3 de los motores);
 
+//El timer 1 lo usamos para dos PWMs (Conectados en PB5 y PB6)
+void setup_timer1(){
+
+	// Prescalado de 8 --> CS5(2:0) = 010
+	// Modo de operacion 10 --> WGM5(3:0) = 1010
+	
+	TCCR1A |= ((1<<COM1A1) | (1<<COM1B1) | (1<<WGM11));
+	TCCR1B |= ((1<<WGM13) | (1<<CS11));
+	
+	//TOP en ICR1 
+	ICR1 = 1000;
+	
+	//OCR1(A y B)
+	OCR1A = d_PWM;
+	OCR1B = d_PWM;
+	
+}
+
+//Configuración del timer 5 (lo uso para el PWM de 3 de los motores);
 void setup_timer5(){
 	
 	// Prescalado de 8 --> CS5(2:0) = 010
