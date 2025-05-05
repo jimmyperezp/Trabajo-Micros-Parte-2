73418ddbcfe9451aea93c36836763ce0635a4140
Jimmy <jaimeperezzp@gmail.com>
2025-05-05 22:51:08 +0200

    Update prueba_movermotor1.cpp

--- Parte 2/Pruebas/prueba_movermotor1.cpp
+++ Parte 2/Pruebas/prueba_movermotor1.cpp
@@ -16,15 +16,47 @@
 #define M1_EN PB5
 #define M1_DI PD4
 
+#define d_PWM 250
 
-
-volatile int fin = 0;
+int fin = 0;
  
- ISR(INT0_vect){			//INT para el SW1
+ ISR(INT0_vect){	//INT para el SW1
 	
 	fin = 1;
 }
 
+void setup(){	
+	
+	//Los Pines de direccion y enable son salidas
+	PORT_M1_EN |= (1 << M1_EN);
+	PORT_M1_DI |= (1 << M1_DI);		
+	
+	EIMSK |= (1 << INT0);  //Habilito las interrupciones INT0 e INT1
+	EICRA |= (1 << ISC01);  //Y hago que salte por flanco de bajada
+		
+	
+	}
+
+void setup_timer1(){   //lo usamos para dos PWMs (Conectados en PB5 y PB6)
+
+	// Prescalado de 8 --> CS5(2:0) = 010
+	// Modo de operacion 10 --> WGM5(3:0) = 1010
+	
+	TCCR1A |= (1<<WGM11);
+	TCCR1B |= ((1<<WGM13) | (1<<CS11));
+	
+	//TOP en ICR1
+	ICR1 = 1000;
+	
+	//OCR1(A y B)
+	OCR1A = d_PWM;
+	OCR1B = d_PWM;
+	
+	//Habilito las interrupciones por coincidencia en OCR1A y OCR1B
+	TIMSK1 |= ((1 << OCIE1A) | (1 << OCIE1B) );
+	
+	
+}
 
 void apagar_motor(){
 	
@@ -35,9 +67,9 @@ void mover_motor1(){
 	
 	apagar_motor();
 	
-	while(fin != 1){
+	while(fin != 1){  	//La 1ª vez, fin=0, así que entra al bucle
 		
-		TCCR1A |= ((1 << COM1A1) | (1 << COM1A0));
+		TCCR1A |= (1 << COM1A1);// | (1 << COM1A0));
 		PORT_M1_DI |= (1 << M1_DI);
 	 }
 	 
@@ -46,25 +78,21 @@ void mover_motor1(){
 	 
 	 while(fin!=1){
 		 
-		TCCR1A |= ((1 << COM1A1) | (1 << COM1A0));
+		TCCR1A |= (1 << COM1A1);// | (1 << COM1A0));
 		PORT_M1_DI  &= ~(1 << M1_DI);
 	}
 	
 	fin = 0;
  }
  
- 
+
  void main (void){
-	 
-	 
-	DDRB |= (1 << M1_EN);
-	DDRD |= (1 << M1_DI);
+	
+	setup();
+	setup_timer1();
 
 	while(1){
 		mover_motor1();
 	}
 	
  }
- 
-	
-
