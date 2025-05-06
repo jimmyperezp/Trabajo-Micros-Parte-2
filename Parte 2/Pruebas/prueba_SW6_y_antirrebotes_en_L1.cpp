931d17f2fdaea6f1a9fd384fb36fa72bac8906d1
Jimmy <jaimeperezzp@gmail.com>
2025-05-06 22:59:28 +0200

    Update prueba_SW6_y_antirrebotes_en_L1.cpp

--- Parte 2/Pruebas/prueba_SW6_y_antirrebotes_en_L1.cpp
+++ Parte 2/Pruebas/prueba_SW6_y_antirrebotes_en_L1.cpp
@@ -11,9 +11,10 @@
 #define L1 PL6
 
 #define SW6 PB7
+#define SW1 PD0
 
-#define REBOTE_MS 50UL       //Número de milisegundos
-#define TICKS_PER_MS 1000 UL // para prescaler 8, 8 MHz → 1 MHz. Número de entradas en la interrupción por milisegundo.
+#define REBOTE_MS 50    //Número de milisegundos
+#define TICKS_PER_MS 1000// para prescaler 8, 8 MHz → 1 MHz. Número de entradas en la interrupción por milisegundo.
 
 
 //VARIABLES DEL ANTIRREBOTES
@@ -22,139 +23,164 @@ volatile uint8_t int_bloqueado[4];  //Bandera para comprobar si la interrupción
 volatile uint8_t bounce_int; 		//Variable auxiliar global para avisar a la interrupción del timer sobre qué interrupción de tipo INT reactivar.
 
 
-
 //Para el control de la PCINT
-volatile uint8_t last_state_SW6;
-volatile uint8_t current_state_SW6;
 
+volatile uint8_t last_state_SW6 = 1 ;
+volatile uint8_t current_state_SW6 = 0;
+
+int habilitar_antirrebotes = 0;
+
+volatile uint8_t high = 2;
+volatile uint8_t low = 0;
+
+void setup(){
+	
+	DDRL |=  (1 << L1);
+	DDRB &= ~(1 << SW6);
+	DDRD &= ~(1 << SW1);
+	
+	high = 1;
+	low = 0;
+	//SI LO CONECTAMOS EN PCINT7:
+	
+	PCICR |= (1 << PCIE0);  //Habilito el grupo de PCINTS de la 0 a la 7 (SW6 está en PCINT7)
+	PCMSK0 |= (1 << PCINT7); // Y habilito sólo la que me interesa (asociada a SW6)
+	
+	EIMSK |= (1 << INT0);		//Habilito INT0
+	EICRA |= (1 << ISC01);		//Y la pongo para que salte en flanco de bajada
+	EICRA &= ~(1 << ISC00);
+
+	
+	PORT_L1 |= (1 << L1);	//Comienzo con la luz encendida
+	
+	
+}
 
 void setup_timer3(){
 	
-		
-		TCCR3A = 0;
-		TCCR3B = (1 << WGM32) | (1 << CS31);  // Modo CTC y prescalado de 8
-		OCR3A  = REBOTE_MS * TICKS_PER_MS;
-		TCNT3  = 0;
-		
-		//Limpio bandera antigua y habilito interrupción del compare
-		TIFR3  |= (1 << OCF3A);
-		TIMSK3 |= (1 << OCIE3A);
+	
+	TCCR3A = 0;
+	TCCR3B = (1 << WGM32) | (1 << CS31);  // Modo CTC y prescalado de 8
+	OCR3A  = REBOTE_MS * TICKS_PER_MS;
+	TCNT3  = 0;
+
+	TIMSK3 |= (1 << OCIE3A);
 }
 
 
 void antirrebotes(uint8_t inum){
 	
 	bounce_int = inum;
+	habilitar_antirrebotes = 1;
 	
 	switch(inum) { //Con este switch, trato de manera distinta las interrupciones por INT y por PCINT
-			
-			
-			case 4:  // antirrebotes en PCINT0 (pin PB0)
-			
-			
-			// Enmascaro PCINT0 para deshabilitar esa fuente de interrupción
-			PCMSK0 &= ~(1<<PCINT0);
-			
-			//Limpio bandera
-			PCIFR  |= (1<<PCIF0); 
-			break;
-
-			case 5:  // antirrebotes en PCINT7 (pin PB7)
-			
-			// Habilito el grupo PCINT0..7 (PCIE0)
-			PCICR  |= (1<<PCIE0);
-			// Enmascaro PCINT7 para deshabilitar esa fuente de interrupción
-			PCMSK0 &= ~(1<<PCINT7);
-			//Limpio bandera
-			PCIFR  |= (1<<PCIF0);
-			break;
-
-			default:
 		
-			EIMSK &= ~(1 << inum);
-			EIFR  |=  (1 << inum);
-			int_bloqueado[inum] = 1; //bandera para ver qué interrupción INT está bloqueada.
-	   break;
-		}
+
+		case 6:  // antirrebotes en PCINT7 (pin PB7)
+
+		PCMSK0 &= ~(1<<PCINT7);
+
+		break;
+
+		default:
+		
+		break;
+		
+	}
 	
+}
+
+
+
+
+
+void cincuenta_ms(){
+	
+	int numero_ciclos = 0;
+	
+	if(habilitar_antirrebotes){
+		
+		numero_ciclos++;
+	}
+	
+	if(numero_ciclos>1){
+		
+		habilitar_antirrebotes = 0;
+		numero_ciclos = 0;
+		
+		switch(bounce_int) {
+			
+			
+			case 5: //reactivo PCINT7
+			
+			PCMSK0 |= (1<<PCINT7);
+			
+			break;
+			
+			default:
+			
+			break;
+		}
+	}
+	
+}
+
+void SW6_flanco(){  //Esta salta en cada flanco de SW6
+	
+
+	antirrebotes(6);
+	current_state_SW6 = ((PINB & (1 << SW6))>> SW6);
+	
+
+
+	if ((last_state_SW6 == high) && (current_state_SW6 == low)) {
+		// Flanco de bajada: PB7 pasó de alto a bajo.
+		PORT_L1 ^= (1 << L1);
+		
+	}
+	
+	last_state_SW6 = current_state_SW6;
+}
+
+
+void SW1_bajada(){
+	
+	PORT_L1 ^= (1 << L1);
 }
 
 
 ISR(TIMER3_COMPA_vect) {
 	
-	// Deshabilito la interrupción de compare.
-	TIMSK3 &= ~(1<<OCIE3A);
 	
-	switch(bounce_int) {
-		
-		case 4: //reactivo PCINT0
-		PCMSK0 |= (1<<PCINT0);
-		break;
-		
-		case 5: //reactivo PCINT7
-		PCMSK0 |= (1<<PCINT7);
-		break;
-		
-		default:
-		// reactivo solo la INT correspondiente
-		EIMSK |= (1<<bounce_int);
-		// 3) La marco como “no bloqueada”. //Esto solo sirve de cara a tener una flag interna.
-		int_bloqueado[bounce_int] = 0;
-		break;
-	}
+	cincuenta_ms();
+
 }
 
-
-
+ISR(PCINT0_vect){
 	
-
-
-void SW6_flanco(){  //Esta salta en cada flanco de SW6
-	
-
-		
-		uint8_t current_state_SW6 = ((PINB & (1 << SW6))>> SW6);  
-		
-		antirrebotes(6);
-		
-		if (last_state_SW6 == 0 && current_state_SW6 == 1) {
-			// Flanco de subida: SW6 pasó de bajo a alto.
-			
-		}
-		else if (last_state_SW6 == 1 && current_state_SW6 == 0) {
-			// Flanco de bajada: PB7 pasó de alto a bajo.	
-
-			PORT_L1 &= ~(1 << L1);
-			
-		}
-		
-		last_state_SW6 = current_state_SW6;
-	}
-	
-	
-
-ISR(PCINT0_VECT){
 	
 	SW6_flanco();
 }
 
+ISR(INT0_vect){
+	
+	SW1_bajada();
+}
 //Programa Principal
 int main(void){
 
-	DDRL |= (1<<L1);
-	DDRB &= ~(1 << SW6);
 
-	PCICR |= (1 << PCIE0);  //Habilito el grupo de PCINTS de la 0 a la 7 (SW6 está en PCINT7)
-	PCMSK0 |= (1 << PCINT7); // Y habilito sólo la que me interesa (asociada a SW6)	
-		
+	setup();
+	
 	setup_timer3();
 	
+	SW6_flanco();
 	while(1){
 		
-	
+		
 	}
 	
-}	
-	
-	
+}
+
+
 
